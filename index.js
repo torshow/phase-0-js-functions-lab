70da801d28eb2663f0db7d1add6f8eb5dd1d7ca2
torshow <slywoods12@gmail.com>
2026-03-23 22:17:27 +0300

    add

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+function calculateTax(amount) {
+    return amount * (10/100);
 
+}
+function convertToUpperCase(test) {
+    return test.toUpperCase();
 
+}
+function findMaximum(num1, num2) {
+    if (num1,num2) {
+        return num1;
+    }  else {
+        return num2;
+    }
+}
+function isPalindrome(word) {
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+}
+function calculateDiscountedPrice(originalPrice, discountpersentage) {
+    const discount = originalPrice * (discountpersentage / 100);
+    return originalPrice - discount;
+
+}
+console.log(calculateTax(100)); //Expected: 10
+console.log(convertToUpperCase("hello")); //Expected: "HELLO"
+console.log(findMaximum(10,20)); //Expected: true
+console.log(calculateDiscountedPrice(200,15)); //Expected: 170
 
 
 
